5576eefa2454fa37ff367a5d349565464b5d5c20
Mar <marmembriveplaza@gmail.com>
2019-11-05 16:20:21 +0100

    Constructor con parámetros de la clase Posicion

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -5,6 +5,13 @@ public class Posicion {
 	private int fila;
 	private char columna;
 
+	//Constructor con parámetros
+	public Posicion (int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
+	
 	public int getFila() {
 		return fila;
 	}

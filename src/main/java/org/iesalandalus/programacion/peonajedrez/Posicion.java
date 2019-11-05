e0e702afc7e0e8d39f1415f75c08abcc906b36a2
Mar <marmembriveplaza@gmail.com>
2019-11-05 16:12:28 +0100

    Get y set de la clase Posicion

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -1,9 +1,38 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else {
+			this.fila = fila;
+		}
+
+	}
+
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else {
+			this.columna = columna;
+		}
+
+	}
+	
+	
+	
 	
 
 }

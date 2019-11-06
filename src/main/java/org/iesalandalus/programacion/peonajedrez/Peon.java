a3622238e036a9c4749293142d05a1c0226664ad
Mar <marmembriveplaza@gmail.com>
2019-11-06 13:45:52 +0100

    get y set de la clase Peon

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -1,9 +1,33 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Peon {
-	
+
 	private Color color;
 	private Posicion posicion;
-	
 
+	public Color getColor() {
+		return color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+
+		this.color = color;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new IllegalArgumentException("ERROR: Mover el peón en una dirección nula no está permitido.");
+		}
+		this.posicion = posicion;
+	}
+
+	
+	
 }

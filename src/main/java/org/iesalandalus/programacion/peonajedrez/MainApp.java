1771db60fc7d47cec6bf8bff83a688f75e0adee2
Mar <marmembriveplaza@gmail.com>
2019-11-13 11:22:41 +0100

    int elegirOpcion()

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -35,13 +35,22 @@ public class MainApp {
 	}
 	
 	private static void mostrarMenu() {
-		  System.out.println("MENÚ DEL PROGRAMA: ");
-		  System.out.println("1: Crear peón por defecto.");
-		  System.out.println("2. Crear peón de un color: blanco o negro.");
-		  System.out.println("3. Crear peón de un color y en una columna inicial dada.");
-		  System.out.println("4. Mover peón.");
-		  System.out.println("5. Salir");
-		}
+		System.out.println("MENÚ DEL PROGRAMA: ");
+		System.out.println("1: Crear peón por defecto.");
+		System.out.println("2. Crear peón de un color: blanco o negro.");
+		System.out.println("3. Crear peón de un color y en una columna inicial dada.");
+		System.out.println("4. Mover peón.");
+		System.out.println("5. Salir");
+	}
 
+	private static int elegirOpcion() {
+		int opcion;
+		do {
+			System.out.println("Por favor, introduzca la opción del menú que desee realizar: ");
+			opcion = Entrada.entero();
+		} while (opcion < 1 || opcion > 5);
+
+		return opcion;
+	}
 
 }

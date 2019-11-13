3d668d49cb4901eea47191f967e076153b2cbfb6
Mar <marmembriveplaza@gmail.com>
2019-11-13 11:20:44 +0100

    void mostrarPeon()

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -1,12 +1,38 @@
 package org.iesalandalus.programacion.peonajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
-	
+
+	static Peon peon = null;
+	static boolean salir = false;
+	static Direccion direccion = null;
+
 	public static void main(String[] args) {
+
 		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
 		System.out.println("-------------------------------------------------------------------------");
-		
-		System.out.println("Hasta luego Lucas!!!!");
+
+		mostrarPeon();
+
+		while (!salir) {
+			//mostrarMenu();
+			//ejecutarOpcion(elegirOpcion());
+		}
+
 	}
-	
+
+	private static void mostrarPeon() {
+		System.out.print("Representación del peón: ");
+		if (peon == null) {
+			System.out.println("Tiene que crear un peón para poder hacer uso de él. ");
+			System.out.println(" ");
+		} else {
+			System.out.println(peon.toString());
+		}
+	}
+
+
 }
